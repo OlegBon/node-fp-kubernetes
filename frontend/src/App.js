dd86deed56f7a5cb19a6bd958a232974f7b6dc4d
OlegBon <oleg_bon@ukr.net>
2025-01-09 17:06:12 +0000

    add pages to front plus App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Users from './pages/Users';
+import Logout from './pages/Logout';
+import Error from './pages/Error';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/users" component={Users} />
+                <Route exact path="/logout" component={Logout} />
+                <Route component={Error} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
