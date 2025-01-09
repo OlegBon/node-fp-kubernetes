6d1f6fa4a21d7e11fa01e033bf2a833caf24befa
OlegBon <oleg_bon@ukr.net>
2025-01-09 19:58:54 +0000

    Fix imports and add dependencies in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Register from './pages/Register';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
+import Register from './pages/Register';
 import Users from './pages/Users';
 import Logout from './pages/Logout';
-import Error from './pages/Error';
 
 function App() {
-    return (
-        <Router>
-            <Switch>
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/users" component={Users} />
-                <Route exact path="/logout" component={Logout} />
-                <Route component={Error} />
-            </Switch>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/logout" element={<Logout />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
