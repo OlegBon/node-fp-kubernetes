51a4612a7ca2dedf68257ae8d9b0abc444d4713f
OlegBon <oleg_bon@ukr.net>
2025-01-09 20:28:35 +0000

    Add home route to frontend and fix backend routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,10 +9,12 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<div>Home Page</div>} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/users" element={<Users />} />
         <Route path="/logout" element={<Logout />} />
+        <Route path="*" element={<div>Page not found</div>} />
       </Routes>
     </Router>
   );
