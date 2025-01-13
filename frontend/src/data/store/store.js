b70498df3cabc2e33d021c368fb4809159733233
OlegBon <oleg_bon@ukr.net>
2025-01-13 18:15:00 +0000

    Optimization Project - store and pages 4

--- frontend/src/data/store/store.js
+++ frontend/src/data/store/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from '../reducers/userSlice';
-import usersReducer from '../reducers/usersSlice';
-import messageReducer from '../reducers/messageSlice';
+import userReducer from '../reducers/userSlice.js';
+import usersReducer from '../reducers/usersSlice.js';
+import messageReducer from '../reducers/messageSlice.js';
 
 const store = configureStore({
   reducer: {
