f883794e76834b52c51370a13c390f0b73908153
OlegBon <oleg_bon@ukr.net>
2025-01-13 14:29:54 +0000

    Add users name on page /Users.js

--- frontend/src/pages/Users.js
+++ frontend/src/pages/Users.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useSelector } from 'react-redux';
 
 const Users = () => {
     const [users, setUsers] = useState([]);
